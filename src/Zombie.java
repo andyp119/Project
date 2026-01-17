642773240580d8273c6a67b96c67e18d5295171d
andyp119 <andyp119@nycstudents.net>
2026-01-16 21:22:55 -0500

    Update Zombie.java

--- src/Zombie.java
+++ src/Zombie.java
@@ -3,22 +3,44 @@ public class Zombie {
     private int health;
     private int damage;
     private int exp;
+    private int zombieFlesh;
 
     public Zombie() {
         health = 50;
         damage = 5;
+        zombieFlesh = (int) (Math.random() * 11) + 5;
         exp = (int) (Math.random() * 50);
     }
 
-    public void getHurt(int dmg) {
-        if (isDead()) {
-            System.out.println("Zombie is already dead");
-        } else {
+    public void level3Zombie() {
+        health = 100;
+        damage = 20;
+        zombieFlesh = (int) (Math.random() * 16) + 20;
+    }
+
+    public void level5Zombie() {
+        health = 300;
+        damage = 50;
+        zombieFlesh = (int) (Math.random() * 16) + 30;
+    }
+
+    public void level7Zombie() {
+        health = 500;
+        damage = 100;
+        zombieFlesh = (int) (Math.random() * 16) + 40;
+    }
+
+    public void takeDamage(int dmg) {
+        if (!isDead()) {
             health -= dmg;
             if (isDead()) {
-                System.out.println("Zombie killed. +" + exp +  " exp");
+                System.out.println("Zombie killed! +" + exp + " exp");
             }
         }
+    }
+
+    public int zombieFleshDrop() {
+        return zombieFlesh;
     }
 
     public int attack() {
